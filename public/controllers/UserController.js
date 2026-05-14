ead907a55193bae7825d9953f9b725fc5af83c0d
Celso Bitello <celsokaiiki@gmail.com>
2026-05-14 00:45:16 -0300

    U05 - Refatorando para uma classe HttpRequest

--- public/controllers/UserController.js
+++ public/controllers/UserController.js
@@ -138,27 +138,13 @@ class UserController {
     }
 
     selectAll() {
-        //let users = User.getUsersStorage();
-        let ajax = new XMLHttpRequest();
-
-            ajax.open('GET', '/users');
-            ajax.onload = event => {
-                let obj = {users: []};    
-                
-                try {
-                    obj = JSON.parse(ajax.responseText);
-
-                } catch (e) {
-                    console.error(e);
-                }
-
-                 obj.users.forEach(dataUser=> {
+        HttpRequest.get('/users').then(data => {
+            obj.users.forEach(dataUser=> {
                     let user = new User();
                     user.loadFromJSON(dataUser);
                     this.addLine(user);
                 })
-            };
-        ajax.send();    
+        }); 
     }
 
     addLine(dataUser) { 
